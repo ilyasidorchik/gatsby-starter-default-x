e712a2b610a3fcef94ec0b54f4ca1dd1f9b7db78
Ilya Sidorchik <ilya@sidorchik.ru>
2020-01-26 13:42:10 +0300

    Indent with tabs 2 symbols width

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,34 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
+import './header.scss'
+
 interface IHeaderProps {
-  siteTitle?: string
+	siteTitle?: string
 }
 
 const Header = ({ siteTitle = '' }: IHeaderProps) => (
-  <header
-    style={{
-      background: `#663399`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#fff`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+	<header className="Header">
+		<div
+			style={{
+				margin: `0 auto`,
+				maxWidth: 960,
+				padding: `1.45rem 1.0875rem`,
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						color: `#fff`,
+						textDecoration: `none`,
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</div>
+	</header>
 )
 
 export default Header
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,40 +12,40 @@ import Header from './header'
 import './layout.scss'
 
 interface ILayoutProps {
-  children: ReactNode
+	children: ReactNode
 }
 
 const Layout = ({ children }: ILayoutProps) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Header siteTitle={data.site.siteMetadata.title} />
+			<div
+				style={{
+					margin: `0 auto`,
+					maxWidth: 960,
+					padding: `0px 1.0875rem 1.45rem`,
+					paddingTop: 0,
+				}}
+			>
+				<main>{children}</main>
+				<footer>
+					© {new Date().getFullYear()}, Built with
+					{` `}
+					<a href="https://www.gatsbyjs.org">Gatsby</a>
+				</footer>
+			</div>
+		</>
+	)
 }
 
 export default Layout
