89121b862e4cb2a9ecde312c3c31ee39d32b5f02
Ilya Sidorchik <ilya@sidorchik.ru>
2020-01-27 12:32:09 +0300

    Refactor Header with BEM classname package

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
-import { Link } from 'gatsby'
 import React from 'react'
+import { Link } from 'gatsby'
+import { cn } from '@bem-react/classname'
 
 import './header.scss'
 
@@ -7,16 +8,20 @@ interface IHeaderProps {
 	siteTitle?: string
 }
 
-const Header = ({ siteTitle = '' }: IHeaderProps) => (
-	<header className="Header">
-		<div className="Header-Content">
-			<h1 className="Header-Title">
-				<Link className="Header-Link" to="/">
-					{siteTitle}
-				</Link>
-			</h1>
-		</div>
-	</header>
-)
+const Header = ({ siteTitle = '' }: IHeaderProps) => {
+	const header = cn('Header')
+
+	return (
+		<header className={header()}>
+			<div className={header('Content')}>
+				<h1 className={header('Title')}>
+					<Link className={header('Link')} to="/">
+						{siteTitle}
+					</Link>
+				</h1>
+			</div>
+		</header>
+	)
+}
 
 export default Header
